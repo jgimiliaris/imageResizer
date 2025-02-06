65661d945f0f656f9435c48acfe1d93de58089fd
John Gimiliaris <johngimiliaris@Johns-MacBook-Pro-2.local>
2025-02-06 13:56:59 +0000

    update bb

--- api/backend.js
+++ api/backend.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
-const { busboy } = require('busboy');
+const busboy = require('busboy');
 
 const app = express();
 // const upload = multer({ dest: 'uploads/' });
